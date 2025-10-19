c9c9043be9eab92ad98aec028c6a6481dbe7f662
danielbarbera1 <danielbar2808@gmail.com>
2025-10-18 20:29:06 -0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import Navbar from './Components/Navbar';
-import Sidebar from './Components/Aside';
+import CategoriesAside from './Components/CategoriesAside';
 import Section from './Components/Section';
 import ProductsSection from './Components/ProductsSection';
 
@@ -9,7 +9,7 @@ function App() {
     <div className="min-h-screen bg-gray-50">
       <Navbar />
       <div className="flex">
-        <Sidebar />
+        <CategoriesAside />
         
         <main className="flex-1">
           <Section />
